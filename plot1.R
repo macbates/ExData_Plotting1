7063f947916b61e63d5c6b8e8477de199e3ae91f
Ken Bates <macbates@me.com>
2014-07-11 13:59:46 -0600

    Initial commit of source and png files

---
+++ plot1.R
@@ -0,0 +1,33 @@
+#
+# Coursera EDA course project 1 - plot1.R
+#
+
+#
+# Since the data file is so large and the project uses only a small subset, a subset of the main file
+# will be used. If it does not exist, the large file will be read in, subset appropriately, and the
+# subset written out. This code is duplicated for all 4 exercises (plot1.R through plot4.R).
+# Note that the original data file must be in the same directory as this code.
+#
+
+fileName <- "subset_data"
+# If the subset file is already there, just read it in
+if (file.exists(fileName)) load(fileName) else {
+  
+  # No file, so we have to read in the full one and create a subset
+  message("Reading full data and subsetting, please wait...")
+  full <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", 
+                     colClasses = c(rep("character", 2), rep("numeric", 7)))
+  
+  # Construct a POSIX date from the (European) date and time fields
+  full$newDate <- strptime(paste(full$Date, full$Time), "%d/%m/%Y %H:%M:%S")
+  loDate <- strptime("2007-02-01", "%Y-%m-%d")
+  hiDate <- strptime("2007-02-03", "%Y-%m-%d")
+  d <- subset(full, full$newDate >= loDate & full$newDate < hiDate)
+  save(d, file=fileName)
+}
+
+# Arrive here with data frame 'd' loaded with the correct dates (d$newDate), so now we can do the plot
+png(filename = "plot1.png")
+hist(d$Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "red")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,48 @@
+#
+# Coursera EDA course project 1 - plot4.R
+#
+
+#
+# Since the data file is so large and the project uses only a small subset, a subset of the main file
+# will be used. If it does not exist, the large file will be read in, subset appropriately, and the
+# subset written out. This code is duplicated for all 4 exercises (plot1.R through plot4.R).
+# Note that the original data file must be in the same directory as this code.
+#
+
+fileName <- "subset_data"
+# If the subset file is already there, just read it in
+if (file.exists(fileName)) load(fileName) else {
+  
+  # No file, so we have to read in the full one and create a subset
+  message("Reading full data and subsetting, please wait...")
+  full <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", 
+                     colClasses = c(rep("character", 2), rep("numeric", 7)))
+  
+  # Construct a POSIX date from the (European) date and time fields
+  full$newDate <- strptime(paste(full$Date, full$Time), "%d/%m/%Y %H:%M:%S")
+  loDate <- strptime("2007-02-01", "%Y-%m-%d")
+  hiDate <- strptime("2007-02-03", "%Y-%m-%d")
+  d <- subset(full, full$newDate >= loDate & full$newDate < hiDate)
+  save(d, file=fileName)
+}
+
+# Arrive here with data frame 'd' loaded with the correct dates (d$newDate), so now we can do the plot
+png(filename = "plot4.png")
+par(mfrow = c(2, 2))
+
+# Top left
+plot(d$newDate, d$Global_active_power, type="l", ylab = "Global Active Power", xlab = "")
+
+# Top right
+plot(d$newDate, d$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+
+# Bottom left
+plot(d$newDate, d$Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering")
+lines(d$newDate, d$Sub_metering_2, col = "red")
+lines(d$newDate, d$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1, bty = "n")
+
+# Bottom right
+plot(d$newDate, d$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
+dev.off()
